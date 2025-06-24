b71d7b7ff4aa083dc1d83176ae34e1ec46c37366
Yazan Alshomali <shomaliyazan7@gmail.com>
2025-06-24 13:00:50 +0300

    Added ex00

---
+++ C02/ex00/ft_strcpy.c
@@ -0,0 +1,24 @@
+#include <unistd.h>
+//#include <stdio.h>
+
+char	*ft_strcpy(char *dest, char *src){
+	int	i;
+	
+	i = 0;
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i]='\0';
+	return (dest);
+}
+/*
+int	main(void)
+{
+	char str[] = "Hello 42";
+	char dest[7];
+	printf("%s\n", ft_strcpy(dest, str));
+	return (0);
+}
+*/
