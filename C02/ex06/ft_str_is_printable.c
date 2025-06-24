6f5814a884a91cc20095427328fb08f5df7a25f8
Yazan Alshomali <shomaliyazan7@gmail.com>
2025-06-24 13:05:29 +0300

    Added ex06

---
+++ C02/ex06/ft_str_is_printable.c
@@ -0,0 +1,26 @@
+#include <unistd.h>
+//#include <stdio.h>
+
+int	ft_str_is_printable(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (str[i] < 32 || str[i] > 126)
+			return (0);
+		i++;
+	}
+	return (1);
+}
+/*
+int	main(void)
+{
+    printf("'Hello' %d\n", ft_str_is_printable("Hello"));
+    printf("'Hello\\n'%d\n", ft_str_is_printable("Hello\n"));
+    printf("'123!@#' %d\n", ft_str_is_printable("123!@#"));
+    printf("'\\tTab'%d\n", ft_str_is_printable("\tTab"));
+    return 0;
+}
+*/
