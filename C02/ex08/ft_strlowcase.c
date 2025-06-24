26b5bcd020a819bf9fc55a5531e25e8ab3265ffa
Yazan Alshomali <shomaliyazan7@gmail.com>
2025-06-24 13:05:38 +0300

    Added ex08

---
+++ C02/ex08/ft_strlowcase.c
@@ -0,0 +1,26 @@
+#include <unistd.h>
+//#include <stdio.h>
+
+char	*ft_strlowcase(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (str[i] >= 'A' && str[i] <= 'Z')
+			str[i]+=32;
+		i++;
+	}
+	return (str);
+}
+
+/*
+int	main(void)
+{
+	char	str[] = "HelLO4235@";
+
+	ft_strlowcase(str);
+	printf("%s", str);
+}
+*/
