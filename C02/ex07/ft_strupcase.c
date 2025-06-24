50eaaf6cb24e735251c75e6dd54663b795cbd07f
Yazan Alshomali <shomaliyazan7@gmail.com>
2025-06-24 13:05:33 +0300

    Added ex07

---
+++ C02/ex07/ft_strupcase.c
@@ -0,0 +1,26 @@
+#include <unistd.h>
+//#include <stdio.h>
+
+char	*ft_strupcase(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+			str[i]-=32;
+	i++;
+	}
+	return (str);
+}
+
+/*
+int	main(void)
+{
+	char	str[] = "Hello42!@";
+
+	ft_strupcase(str);
+	printf("%s", str);
+}
+*/
