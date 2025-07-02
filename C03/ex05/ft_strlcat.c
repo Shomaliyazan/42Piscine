88b95404cff0cdf85dbd0f57284cb5418d002e87
Yazan Alshomali <shomaliyazan7@gmail.com>
2025-07-02 22:15:27 +0300

    finished C03

--- C03/ex05/ft_strlcat.c
+++ C03/ex05/ft_strlcat.c
@@ -1,43 +1,59 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: shomali <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/02 22:11:33 by shomali           #+#    #+#             */
+/*   Updated: 2025/07/02 22:12:20 by shomali          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <unistd.h>
 //#include <stdio.h>
 
-int	ft_strlen(char *str)
+unsigned int	ft_strlen(char *str)
 {
-	int	i;
+	unsigned int	len;
 
-	i = 0;
-	while (str[i] != '\0')
-		i++;
-	return (i);
+	len = 0;
+	while (str[len] != '\0')
+		len++;
+	return (len);
 }
 
 unsigned int	ft_strlcat(char *dest, char *src, unsigned int size)
 {
-	int	i;
-	int	j;
-	int	k;
+	unsigned int	dest_len;
+	unsigned int	src_len;
+	unsigned int	i;
 
+	dest_len = ft_strlen(dest);
+	src_len = ft_strlen(src);
 	i = 0;
-	j = ft_strlen(dest);
-	k = ft_strlen(src);
-	if (j >= size)
-		return (k + size);
-	while (src[i] != '\0' && j + i < size - 1)
+	if (dest_len >= size)
+		return (src_len + size);
+	while (src[i] != '\0' && dest_len + i < size - 1)
 	{
-		dest[i + j] = src[i];
+		dest[dest_len + i] = src[i];
 		i++;
 	}
-	if (j + k < size)
-		dest[j + i] = '\0';
-	return (j + k);
+	dest[dest_len + i] = '\0';
+	return (dest_len + src_len);
 }
 /*
 int main()
 {
-	char dest[] = "Hello people";
-	char src[] = "and fellaw programmers";
-	unsigned int size = 25;
-	printf("%d\n",ft_strlcat(dest, src, size));
-	return 0;
+    char dest[25] = "Hello ";
+    char src[] = "world!";
+    unsigned int size = 25;
+    
+    printf("Before: %s\n", dest);
+    unsigned int result = ft_strlcat(dest, src, size);
+    printf("After: %s\n", dest);
+    printf("Return: %u\n", result);
+    
+    return 0;
 }
 */
